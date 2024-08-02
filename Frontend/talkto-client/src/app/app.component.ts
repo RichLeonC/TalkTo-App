201817d9bd651a75325f967a3491b251ad6cfce3
RichLeonC <r29leonc@gmail.com>
2024-08-02 16:34:16 -0600

    Building Login UI

--- Frontend/talkto-client/src/app/app.component.ts
+++ Frontend/talkto-client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from "./auth/sign-in/sign-in.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'talkto-client';
+ 
 }
