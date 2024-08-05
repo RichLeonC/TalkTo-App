f7ca986e82f4a790211196912d64b1b746c59f49
RichLeonC <r29leonc@gmail.com>
2024-08-05 17:00:29 -0600

    Progress Login UI

--- Frontend/talkto-client/src/app/auth/sign-in/sign-in.component.ts
+++ Frontend/talkto-client/src/app/auth/sign-in/sign-in.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+  signInForm;
 
+  constructor(private fb: FormBuilder) {
+    this.signInForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  signIn() {
+    console.log(this.signInForm.value);
+  }
+
+  onSubmit() {
+    this.signIn();
+  }
 }
